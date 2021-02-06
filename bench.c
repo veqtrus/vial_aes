ba105f8740efcdd24d4d583e2e8dbc0e7c36b478
Paul Georgiou <pavlos1998@gmail.com>
2021-02-06 00:36:55 +0000

    Benchmark CTR mode

--- bench.c
+++ bench.c
@@ -1,5 +1,14 @@
+/* SPDX-License-Identifier: BSL-1.0
+Copyright (c) 2021 Pavlos Georgiou
+
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or copy at
+https://www.boost.org/LICENSE_1_0.txt
+*/
+
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "aes.h"
 
@@ -9,17 +18,23 @@
 #include <x86intrin.h>
 #endif
 
+#define BUFFER_SIZE 4096
+
 int main()
 {
 	struct vial_aes_key key;
-	struct vial_aes_block block;
+	struct vial_aes aes;
+	uint8_t *buffer = malloc(BUFFER_SIZE);
+	for (unsigned i = 0; i < BUFFER_SIZE; ++i)
+		buffer[i] = i;
 	vial_aes_key_init(&key, 128, (const uint8_t *) "0123456789ABCDEF");
+	vial_aes_init(&aes, VIAL_AES_MODE_CTR, &key, (const uint8_t *) "ABCDEF654321", 12);
 	clock_t dur = clock();
 	uint64_t tsc = __rdtsc();
-	for (uint32_t i = 0; i < 1000000; ++i)
-		vial_aes_block_encrypt(&key, (uint8_t *) &block, (uint8_t *) &block);
+	vial_aes_encrypt(&aes, buffer, buffer, BUFFER_SIZE);
 	tsc = __rdtsc() - tsc;
 	dur = clock() - dur;
-	printf("AES encryption speed: %f MB/s; %f cpb\n", 16.0 * CLOCKS_PER_SEC / dur, tsc / 16000000.0);
+	free(buffer);
+	printf("AES-CTR encryption speed: %f MB/s; %f cpb\n", (BUFFER_SIZE / 1.0e6) * CLOCKS_PER_SEC / dur, tsc / (double) BUFFER_SIZE);
 	return 0;
 }
